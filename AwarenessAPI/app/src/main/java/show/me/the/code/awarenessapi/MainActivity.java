a7abb33802579bc218e296c40edd4a9ef02d74ce
bruno campos <camposbrunocampos@gmail.com>
2016-10-04 20:53:22 -0300

    1st step) Create an instance of Google Awareness API in your activitiy

--- AwarenessAPI/app/src/main/java/show/me/the/code/awarenessapi/MainActivity.java
+++ AwarenessAPI/app/src/main/java/show/me/the/code/awarenessapi/MainActivity.java
@@ -3,11 +3,19 @@ package show.me.the.code.awarenessapi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.awareness.Awareness;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        android.content.Context context;
+        GoogleApiClient client = new GoogleApiClient.Builder(this)
+                .addApi(Awareness.API)
+                .build();
+        client.connect();
     }
 }
