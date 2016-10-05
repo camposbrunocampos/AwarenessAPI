78759b8cccb78658f51197088216d80a80030d27
bruno campos <camposbrunocampos@gmail.com>
2016-10-04 21:25:51 -0300

    3rd step) registering fence

--- AwarenessAPI/app/src/main/java/show/me/the/code/awarenessapi/MainActivity.java
+++ AwarenessAPI/app/src/main/java/show/me/the/code/awarenessapi/MainActivity.java
@@ -1,14 +1,22 @@
 package show.me.the.code.awarenessapi;
 
+import android.app.PendingIntent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.awareness.Awareness;
 import com.google.android.gms.awareness.fence.AwarenessFence;
 import com.google.android.gms.awareness.fence.DetectedActivityFence;
+import com.google.android.gms.awareness.fence.FenceUpdateRequest;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PendingIntent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +30,14 @@ public class MainActivity extends AppCompatActivity {
 
         AwarenessFence walkingFence = DetectedActivityFence.during(DetectedActivityFence.WALKING);
 
+        Awareness.FenceApi.updateFences(client, new FenceUpdateRequest.Builder().addFence("walkingFence", walkingFence, intent).build())
+                .setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        if (status.isSuccess()) {
+                            Toast.makeText(MainActivity.this, "Fence has been registered", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
